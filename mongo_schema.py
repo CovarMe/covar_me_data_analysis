7777deb6a4847e212a7b2ba04341a8ecf3823aed
Westermann <westermann93@googlemail.com>
2016-11-11 16:57:52 +0100

    Update mongo_schema.py

--- mongo_schema.py
+++ mongo_schema.py
@@ -2,17 +2,17 @@
 from app import db
 
 class Stock(db.Document):
-    ticker = db.StringField(required=True,max_length=120)
+    ticker = db.StringField(required=True, max_length=4)
 
 
 class Portfolio(db.Document):
-    name = db.StringField(required=True,max_length=120)
+    name = db.StringField(required = True, max_length=120)
     user = db.ReferenceField(User)
     stocks = ListField(ReferenceField(Stock))
 
 
 class User(db.Document):
-    email = db.EmailField(unique=True)
-    password = db.StringField(default=True)
+    email = db.EmailField(unique = True)
+    password = db.StringField(required = True, default = True, min_length=8)
     timestamp = db.DateTimeField(default=datetime.datetime.now())
     portfolios = ListField(ReferenceField(Portfolio))
