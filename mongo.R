4c73c1fca59b78e629094ac5bacb58543be2f18b
Santiago Paz <santiago.l.paz@gmail.com>
2016-11-27 22:05:18 +0100

    adding rmongodb install guide

--- mongo.R
+++ mongo.R
@@ -1,5 +1,10 @@
 library(rmongodb)
 
+# Installing rmongodb. Download latest package from CRAN at https://cran.r-project.org/src/contrib/Archive/rmongodb/
+# Check where your package library is with .libPaths(). Should be [1]
+# Make sure zip folder is in your current directory then run 
+# install.packages('rmongodb_1.8.0.tar.gz', lib="Library",repos = NULL)
+
 matrix.test <- as.data.frame(matrix(rnorm(1000*1000),1000,1000))
 
 matrix <- as.data.frame(matrix)
